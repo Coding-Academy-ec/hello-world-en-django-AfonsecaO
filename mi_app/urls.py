0cf7afd4199b7a4a3de0cd09ef4420e844d6f98b
Alan Fonseca <afonsecao@outlook.com>
2024-04-14 21:03:38 -0500

    Mejoras

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import hello
+from .views import hello_world
 
 urlpatterns = [
     # define la ruta para la vista hello_world.
-    path ("app/", hello)
+    path ("mi_app/", hello_world)
     
 ]
